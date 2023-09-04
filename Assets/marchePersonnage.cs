abf44569a5884b82bb5037e4b70c28309255c79c
06 <davidndonda@icloud.com>
2023-09-04 06:01:49 -0700

    ajout du systeme temporel

--- Assets/marchePersonnage.cs
+++ Assets/marchePersonnage.cs
@@ -7,9 +7,11 @@ public class marchePersonnage : MonoBehaviour
     // Start is called before the first frame update
     public float speed = 5f;
     Rigidbody2D rb;
+    Animator anim;
     void Start()
     {
       rb=GetComponent<Rigidbody2D>();
+      anim=GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -18,6 +20,29 @@ public class marchePersonnage : MonoBehaviour
      float moveHorizontal = Input.GetAxis("Horizontal");
      float moveVertical = Input.GetAxis("Vertical");
      Vector2 move= new Vector2(moveHorizontal, moveVertical);
-     rb.velocity = move*speed;
+     rb.velocity = move*speed*Time.fixedDeltaTime;
+     makeAnimation(moveHorizontal, moveVertical);
+    }
+    void makeAnimation(float dirx, float diry){
+      if(dirx==0 && diry==0)
+      {
+        anim.SetInteger("dir",0);
+      }
+      else if(dirx>0)
+      {
+        anim.SetInteger("dir", 2);
+      }
+      else if(dirx<0)
+      {
+        anim.SetInteger("dir", 1);
+      }
+      else if(diry>0)
+      {
+        anim.SetInteger("dir", 3);
+      }
+      else if(diry<0)
+      {
+        anim.SetInteger("dir", 4);
+      }
     }
 }
---
+++ Assets/script/ui.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ui : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    int time;
+    [SerializeField]
+    Text myTime;
+    void Start()
+    {
+     StartCoroutine(Minuterie());   
+    }
+    IEnumerator Minuterie (){
+         
+        while(time>0){
+            yield return new WaitForSeconds(1f);
+            time--;
+            Debug.Log("Minuterie du temps"+time);
+            myTime.text="Time chronos : "+time;
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
